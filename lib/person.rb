e9d7f91634e76aa8e3e08c1fddf053b41ccd0b46
github username <github email address>
2019-08-28 10:33:34 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,107 @@
-# your code goes here
+class Person
+    attr_reader :name, :happiness, :hygiene
+    attr_accessor :bank_account
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(happiness)
+        if happiness > 10
+            @happiness = 10
+        elsif happiness < 0
+            @happiness = 0
+        else
+            @happiness = happiness
+        end
+        
+    end
+
+    def hygiene=(hygiene)
+        if hygiene > 10
+            @hygiene = 10
+        elsif hygiene < 0
+            @hygiene = 0
+        else
+            @hygiene = hygiene
+        end
+    end
+
+    def clean?
+        @hygiene > 7
+    end
+
+    def happy?
+        @happiness > 7
+    end
+
+    def get_paid(salary)
+        @bank_account += salary
+        "all about the benjamins"
+    end
+
+    def take_bath
+        # if @hygiene < 7
+        #     @hygiene += 4
+        # else
+        #     @hygiene = 10
+        # end
+        # "♪ Rub-a-dub just relaxing in the tub ♫"
+        self.hygiene+=4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        # if @happiness < 9
+        #     @happiness += 2
+        # else
+        #     @happiness = 10
+        # end
+
+        # if @hygiene < 3
+        #     @hygiene = 0
+        # else
+        #     @hygiene -= 3
+        # end
+        self.happiness +=2
+        self.hygiene -= 3
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        # if @happiness > 7
+        #     @happiness = 10
+        # else
+        #     @happiness += 3
+        # end
+        self.happiness += 3
+
+        # if friend.happiness > 7
+        #     friend.happiness = 10
+        # else
+        #     friend.happiness += 3
+        # end
+        friend.happiness += 3
+        "Hi #{friend.name}! It's #{@name}. How are you?"
+    end
+
+    def start_conversation(person,topic)
+        if topic == "politics"
+            self.happiness -= 2
+            person.happiness -= 2
+            return "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            self.happiness += 1
+            person.happiness += 1
+            return "blah blah sun blah rain"
+        else
+            return "blah blah blah blah blah"
+        end
+
+    end
+
+
+
+end
